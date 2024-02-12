75a7e4e26c9228c160d62c42a9c2e794d65928bc
h3h3 <h3h3.one@gmail.com>
2024-02-12 19:02:28 +0300

    add image zoom

--- public/javascripts/album.js
+++ public/javascripts/album.js
@@ -1,7 +1,7 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('wp-post-image');
+var img = document.querySelector('.wp-post-image');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
